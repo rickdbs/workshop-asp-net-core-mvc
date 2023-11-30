60728b2906111130f8b9504cc490a4dbdd66ece6
Henrique <rickdbs.dev@hotmail.com>
2023-11-30 15:55:58 -0300

    Adicionar arquivos de projeto.

---
+++ SallesWebMvc/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace SallesWebMvc.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
